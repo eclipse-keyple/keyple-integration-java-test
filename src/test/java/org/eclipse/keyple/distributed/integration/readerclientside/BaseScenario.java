cccdd8f0c29d7717e2cb01c4685ba3cbe74addb9
VincentDucrot <vincent.ducrot@cis-france.net>
2026-03-17 11:08:43 +0100

    Reorganize imports to standardize order across test classes

--- src/test/java/org/eclipse/keyple/distributed/integration/readerclientside/BaseScenario.java
+++ src/test/java/org/eclipse/keyple/distributed/integration/readerclientside/BaseScenario.java
@@ -29,7 +29,6 @@ import org.eclipse.keyple.distributed.integration.readerclientside.model.OutputD
 import org.eclipse.keyple.distributed.integration.util.NamedThreadFactory;
 import org.eclipse.keyple.distributed.spi.AsyncEndpointServerSpi;
 import org.eclipse.keyple.plugin.stub.*;
-import org.eclipse.keypop.genericcard.GenericCardSelectionExtension;
 import org.eclipse.keypop.reader.CardReader;
 import org.eclipse.keypop.reader.ConfigurableCardReader;
 import org.eclipse.keypop.reader.ReaderApiFactory;
@@ -38,6 +37,7 @@ import org.eclipse.keypop.reader.selection.CardSelectionResult;
 import org.eclipse.keypop.reader.selection.CardSelector;
 import org.eclipse.keypop.reader.selection.IsoCardSelector;
 import org.eclipse.keypop.reader.selection.spi.SmartCard;
+import org.eclipse.keypop.genericcard.GenericCardSelectionExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
